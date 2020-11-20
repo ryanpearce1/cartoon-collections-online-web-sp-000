b62e1bfa9fb4f80609c1abde5814087719ec5d63
Ryan Pearce <ryan.pearce@key.me>
2020-11-20 21:47:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def long_planeteer_calls(yeeeeer)
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.find do cheese
+  cheese.find do |cheese|
     cheese_types.include?(cheese)
  end
 end
